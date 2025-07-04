1a173f4b7683b1c83e16ed426d7479a8aa8525d7
Serge Zaugg <serge.zaugg@gmail.com>
2025-07-04 16:56:33 +0200

    updated sample_code.py - now 3 concrete usage examples

--- sample_code.py
+++ sample_code.py
@@ -10,75 +10,56 @@ import os
 import xeno_canto_organizer.xco as xco
 # import src.xeno_canto_organizer.xco as xco
 
-# make a projects dir, if it does not already exist
-if not os.path.isdir('./temp_xc_project'):
-    os.makedirs('./temp_xc_project')
+# helper code to generate project dirs
+def make_temp_dir(tag):
+    if not os.path.isdir('./temp_xc_project_' + tag):
+        os.makedirs('./temp_xc_project_' + tag)
+make_temp_dir('01')
+make_temp_dir('02')
+make_temp_dir('03')
 
-# Make an instance of the XCO class and define the start path 
-xc = xco.XCO(start_path = './temp_xc_project')
 
-# custom queries can be run sequentially - xc.recs_pool accumulates the meta-data 
+#---------------------------------
+# (Example 1) custom search can be run sequentially - xc.recs_pool accumulates the meta-data 
 # On first use of download_summary xc api key must be provided
-xc.download_summary(gen = "Corvus", cnt = "switzerland", q = "A", len_max = 14, verbose=True)
+xc = xco.XCO(start_path = './temp_xc_project_01')
+xc.download_summary(gen = "Corvus", sp = "corax", cnt = "switzerland", q = "A", len_max = 200, verbose=True)
+xc.download_summary(gen = "Pyrrhocorax", cnt = "France", q = "B", len_min = 10, verbose=True)
+xc.download_summary(gen = "Coloeus", cnt = "Belgium", q = "C", len_max = 100 , verbose=True)
 print(len(xc.recs_pool))
-xc.download_summary(gen = "Pyrrhocorax", cnt = "switzerland", q = "B", len_min = 10, verbose=True)
-print(len(xc.recs_pool))
-xc.download_summary(gen = "Coloeus", cnt = "switzerland", q = "A",len_max = 100 , verbose=True)
-print(len(xc.recs_pool))
-xc.download_summary(fam = "Corvidae", q = "A",len_max = 10 , verbose=True)
-print(len(xc.recs_pool))
-xc.download_summary(gen = "Corvus", area = "Europe", q = "A", smp_min = 23000, smp_max = 24001, verbose=True)
-print(len(xc.recs_pool))
-
-# sequential queries can be wrappd into a loop 
-for g in ["Corvus", "Pyrrhocorax", "Coloeus", "Garrulus", "Pica"]:
-    xc.download_summary(gen = g, cnt = "France", q = "A", len_min = 5, len_max = 10)
-    print("Cumulative N files: ", len(xc.recs_pool))
-
-# compile all content gathered above into a single df
 xc.compile_df_and_save(verbose = True)
-# check 
-xc.df_recs.shape
-print(xc.df_recs['gen'].value_counts())
-print(xc.df_recs['q'].value_counts())
-print(xc.df_recs['lic'].value_counts())
-print(xc.df_recs['smp'].value_counts())
-
+# get some summaries 
 print(xc.df_recs['full_spec_name'].value_counts())
+print(xc.df_recs['gen'].value_counts())
+print(xc.df_recs['cnt'].value_counts())
 # if session was closed
 xc.reload_local_summary()
-# Download the files 
+# Download the mp3 files 
 xc.download_audio_files(verbose=True)
 # Convert mp3s to wav with a specific sampling rate (requires ffmpeg to be installed)
 xc.mp3_to_wav(conversion_fs = 24000)
 # Extract spectrograms from segments and store as PNG
-xc.extract_spectrograms(
-    fs_tag = 24000, 
-    segm_duration = 2.0, 
-    segm_step = 0.5, 
-    win_siz = 512, 
-    win_olap = 256, 
-    max_segm_per_file = 10, 
-    specsub = True, 
-    log_f_min = 0.02,
-    colormap='viridis',
-    verbose=True
-    )
+xc.extract_spectrograms(fs_tag = 24000, segm_duration = 2.0, segm_step = 0.5, win_siz = 512, win_olap = 256, 
+    max_segm_per_file = 10, specsub = True, log_f_min = 0.02, colormap='viridis',verbose=True)
 
-#---------------------------------------
-# Open a new session
-import xeno_canto_organizer.xco as xco
-# The pre-downloaded mp3 files can be reprocessed with different parameters 
-# Point XCO to the dir with pre-downloaded mp33
-xc = xco.XCO(start_path = './temp_xc_project')
-# Make wavs with fs = 20000 and then short spectrogram 
+
+#---------------------------------
+# (Example 2) search can be based on family and larger areas + sampling rate limits 
+xc = xco.XCO(start_path = './temp_xc_project_02')
+xc.download_summary(fam = "Corvidae", area = "Europe", smp_min = 23000, smp_max = 24001,  len_max = 10, verbose=True)
+xc.compile_df_and_save(verbose = True)
+xc.download_audio_files(verbose=True)
 xc.mp3_to_wav(conversion_fs = 20000)
-xc.extract_spectrograms(fs_tag = 20000, segm_duration = 0.202, segm_step = 0.5, win_siz = 256, win_olap = 220.5, max_segm_per_file = 20, 
-                        log_f_min = None,
-                        specsub = True, colormap='gray')
+xc.extract_spectrograms(fs_tag = 20000, segm_duration = 0.202, segm_step = 0.5, win_siz = 256, win_olap = 220.5, 
+                        max_segm_per_file = 20, specsub = True, log_f_min = None, colormap='gray')
 
-# Make  Make wavs with fs = 16000 and then long spectrogram 
+#---------------------------------
+# (Example 3) sequential queries can be wrapped into a loop 
+xc = xco.XCO(start_path = './temp_xc_project_03')
+for g in ["Corvus", "Pyrrhocorax", "Coloeus", "Garrulus", "Pica"]:
+    xc.download_summary(gen = g, cnt = "France", q = "A", len_min = 5, len_max = 8)
+xc.compile_df_and_save(verbose = True)
+xc.download_audio_files(verbose=True)
 xc.mp3_to_wav(conversion_fs = 16000)
-xc.extract_spectrograms(fs_tag = 16000, segm_duration = 1.738, segm_step = 0.95, win_siz = 256, win_olap = 220.00, max_segm_per_file = 20, 
-                        log_f_min = 0.005,
-                        specsub = False, colormap='viridis')
+xc.extract_spectrograms(fs_tag = 16000, segm_duration = 1.738, segm_step = 0.95, win_siz = 256, win_olap = 220.00, 
+                        max_segm_per_file = 20, log_f_min = 0.005, specsub = False, colormap='viridis')
